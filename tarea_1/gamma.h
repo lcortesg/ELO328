4f125d8ea9e5eaa0caf38aa37621f731e75bcf4b
Lucas Cortés <lucas.cortes.14@sansano.usm.cl>
2020-09-12 10:40:28 -0300

    Creación de archivos de cabecera

---
+++ tarea_1/gamma.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <stdio.h>
+#include <iostream>
+#include <math.h> 
+#include <time.h>
+#include <chrono> 
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace std;
+using namespace cv;
+using namespace chrono; 
+
+// Variables globales.
+#define borde 50 // Ancho del borde.
+#define invertir false // Invierte la imagen horizontalmente.
+#define timer true // Muestra el timer.
+bool tabla = false, pixel = false, imagen = false, video = false; // Tipo de procesamiento a realizar.
+int R = 0, G = 0, B = 0, X = 0, Y = 0, W, H; // Color borde, posición, ancho y alto de ventana.
+float gamma_value; // Valor de corrección Gamma.
+cv::Mat img, img_out, img_border; // Matrices de imágenes.
+
+void corregir_tabla(cv::Mat& img, float gamma_value){ // Función de corrección por tabla.
+	unsigned char gamma_table[256]; // Creación de la tabla Gamma.
+	for (int i = 0; i < 256; i++) gamma_table[i] = cv::saturate_cast<uchar>(pow((float)(i / 255.0), gamma_value) * 255.0f);
+	cv::MatIterator_<uchar> it, end;
+	for (it = img.begin<uchar>(), end = img.end<uchar>(); it != end; it++) *it = gamma_table[(*it)];
+} 
+
+void corregir_pixel(cv::Mat& img, float gamma_value){ // Función de correción pixel a pixel.
+	cv::MatIterator_<uchar> it, end;
+	for (it = img.begin<uchar>(), end = img.end<uchar>(); it != end; it++) *it = cv::saturate_cast<uchar>(pow((float)(*it/255.0), gamma_value) * 255.0f);
+}
+
+void corregir(cv::Mat& img){
+    cv::cvtColor(img, img_out, cv::COLOR_BGR2YCrCb); // Se convierte la imagen desde BGR a YUV.
+    cv::Mat whole = img_out; // Imagen en YUV.
+	cv::Mat img_part(
+	whole,
+	cv::Range( Y, Y+H ), // rows.
+	cv::Range( X, X+W )); // cols.
+	
+	auto start = high_resolution_clock::now(); // Se inicia el timer.
+	if (tabla) corregir_tabla(img_part, gamma_value); // Corrección Gamma con tabla.
+    if (pixel) corregir_pixel(img_part, gamma_value); // Corrección Gamma con función.
+	auto stop = high_resolution_clock::now(); // Se detiene el timer.
+	auto duration = duration_cast<microseconds>(stop - start); // Se calcula la duración del timer.
+	if(timer) cout<<"Tiempo de conversión de imagen: "<<duration.count()<<" microsegundos"<< endl;
+
+	cv::cvtColor(img_part, img_part, cv::COLOR_YCrCb2BGR); // Se convierte la imagen desde color YUB a BGR.
+	img_part.copyTo(img(cv::Rect(X, Y, img_part.cols, img_part.rows))); // Se fusiona la ventana corregida.
+	cv::Scalar value(B,G,R); // Se genera vector de valores BGR;
+	copyMakeBorder(img, img_border, borde, borde, borde, borde, cv::BORDER_CONSTANT, value); // Se genera el borde.
+	cv::imshow("Imagen con correción Gamma en capa de luminancia", img_border); // Se muestra la imagen corregida.
+}
--- tarea_1/main.cpp
+++ tarea_1/main.cpp
@@ -2,67 +2,12 @@
  * @brief: Tarea 1.
  * @author: Lucas Cortés G., Aline Covarrubias G., Mathias Sanhueza L., Christian Pflaumer A.
  * @date: 2020/09/11
- * @compiler: g++ $(pkg-config --cflags --libs opencv4) -std=c++11 main.cpp -o GAMMA
+ * @compiler: g++ -Wall $(pkg-config --cflags --libs opencv4) -std=c++11 main.cpp -o GAMMA
  */
 
+#include "gamma.h"
 #include <stdio.h>
-#include <math.h> 
-#include <time.h>
-#include <chrono> 
 #include <iostream>
-#include <opencv2/opencv.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/videoio.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-
-using namespace std;
-using namespace cv;
-using namespace chrono; 
-
-// Variables globales.
-const int borde = 50; // Ancho del borde.
-bool invertir = false; // Invierte la imagen horizontalmente.
-bool timer = true; // Muestra el timer.
-bool tabla = false, pixel = false, imagen = false, video = false; // Tipo de procesamiento a realizar.
-int R = 0, G = 0, B = 0, X = 0, Y = 0, W, H; // Color borde, posición, ancho y alto de ventana.
-float gamma_value; // Valor de corrección Gamma.
-cv::Mat img, img_out, img_border; // Matrices de imágenes.
-
-void corregir_tabla(Mat& img, float gamma_value){ // Función de corrección por tabla.
-	unsigned char gamma_table[256]; // Creación de la tabla Gamma.
-	for (int i = 0; i < 256; i++) gamma_table[i] = saturate_cast<uchar>(pow((float)(i / 255.0), gamma_value) * 255.0f);
-	MatIterator_<uchar> it, end;
-	for (it = img.begin<uchar>(), end = img.end<uchar>(); it != end; it++) *it = gamma_table[(*it)];
-} 
-
-void corregir_pixel(Mat& img, float gamma_value){ // Función de correción pixel a pixel.
-	MatIterator_<uchar> it, end;
-	for (it = img.begin<uchar>(), end = img.end<uchar>(); it != end; it++) *it = saturate_cast<uchar>(pow((float)(*it/255.0), gamma_value) * 255.0f);
-}
-
-void corregir(){
-    cv::cvtColor(img, img_out, cv::COLOR_BGR2YCrCb); // Se convierte la imagen desde BGR a YUV.
-    cv::Mat whole = img_out; // Imagen en YUV.
-	cv::Mat img_part(
-	whole,
-	cv::Range( Y, Y+H ), // rows.
-	cv::Range( X, X+W ));// cols.
-	
-	auto start = high_resolution_clock::now();// Se inicia el timer.
-	if (tabla) corregir_tabla(img_part, gamma_value);// Corrección Gamma con tabla.
-    if (pixel) corregir_pixel(img_part, gamma_value); // Corrección Gamma con función.
-	auto stop = high_resolution_clock::now(); // Se detiene el timer.
-	auto duration = duration_cast<microseconds>(stop - start); // Se calcula la duración del timer.
-	if(timer) cout<<"Tiempo de conversión de imagen: "<<duration.count()<<" microsegundos"<< endl;
-
-	cv::cvtColor(img_part, img_part, cv::COLOR_YCrCb2BGR);// Se convierte la imagen desde color YUB a BGR.
-	img_part.copyTo(img(cv::Rect(X, Y, img_part.cols, img_part.rows))); // Se fusiona la ventana corregida.
-	Scalar value(B,G,R); // Se genera vector de valores BGR;
-	copyMakeBorder(img, img_border, borde, borde, borde, borde, BORDER_CONSTANT, value); // Se genera el borde.
-	cv::imshow("Imagen con correcion Gamma en capa de luminancia", img_border); // Se muestra la imagen corregida.
-}
-
 
 int main(int argc, char *argv[]){
 
@@ -138,29 +83,29 @@ int main(int argc, char *argv[]){
         		return 1;
             }   
         }
-        cout << "Se inicia la conversion" << endl << "Presionar cualquier tecla para salir" << endl;
-        corregir();// Se corrige la imagen.
+        cout << "Se inicia la conversión" << endl << "Presionar cualquier tecla para salir" << endl;
+        corregir(img); // Se corrige la imagen.
     }
         
     if (video){ // Lógica de procesamiento de video.
 
-        VideoCapture cap; // Captura de video.
+        cv::VideoCapture cap; // Captura de video.
         //cap.open(0); // Abre la cámara default usando la API por default.
         int deviceID = 0; // 0 = Cámara por default.
         int apiID = cv::CAP_ANY; // 0 = Autodetectar API por default.
         cap.open(deviceID, apiID); // Abrir la cámara seleccionada usando la API seleccionada.
         
-        if (!cap.isOpened()) { // Revisar si no hay errores
+        if (!cap.isOpened()) { // Revisar si no hay errores.
             cerr << "Error abriendo camara\n";
             return -1;
         }
         
-        cout << "Se inicia la grabacion" << endl << "Presionar cualquier tecla para salir" << endl;
+        cout << "Se inicia la grabación" << endl << "Presionar cualquier tecla para salir" << endl;
 
         for (;;){ // Loop de captura.
 
             cap.read(img); // Esperar por un nuevo frame.
-            if(invertir) cv::flip(img, img, 1); // Invierte la imagen horizontalmente.
+            if(invertir) cv::flip(img, img, 1); // Invierte la imagen horizontalmente si "invertir" es verdadero.
             W = img.cols;
         	H = img.rows;
 
@@ -199,13 +144,11 @@ int main(int argc, char *argv[]){
         			return 1;
             	}
             }
-            corregir(); // Se corrige la imagen.
-            if (waitKey(5) >= 0) break;
+            corregir(img); // Se corrige la imagen.
+            if (cv::waitKey(5) >= 0) break;
         }
         return 0;
     }
-    waitKey(0);
+    cv::waitKey(0);
     return 0;
 }
-
-
